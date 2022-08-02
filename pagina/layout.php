f48adb5a797fc72d0c2f1940ccf4cee6c49a8cb1
Alan Nogueira <alan.nogueira40@hotmail.com>
2022-08-02 12:01:19 -0300

    Modifica os button do menu e falta fazer a parte de login

--- pagina/layout.php
+++ pagina/layout.php
@@ -1,26 +1,27 @@
 <div class="area-principal">
 
     <div class="menu">
-        <img src="imagens-roupas1/logotipo.jpeg" alt="">
+        <img src="imagens-roupas1/logotipo.jpeg" alt="" style="float: left;">
         
-<span class="links-pag">
-        <a href="">Inicio</a> |
-        <a href="">Quem-Somos</a> |
-        <a href="">Camisas</a> |
-        <a href="">Contato</a> 
-</span>
+        <div class="links-pag">
+            <a href="">Inicio</a> |
+            <a href="">Quem-Somos</a> |
+            <a href="">Camisas</a> |
+            <a href="">Contato</a> 
+        </div>
+        
+        
+        <span class="form-login">
+            <form action="" method="post">
+                <label for="" style="margin-right: 7px;">Email:</label>
+                <input type="text" name="email">
+                <a href="">Cadastrar</a><br>
+                <label for="">Senha:</label>
+                <input type="text" name="senha">
+                <button type="submit">Entrar</button>
+            </form>
 
-    <span class="form-login">
-        <form action="" method="post">
-            <label for="" style="margin-right: 7px;">Email:</label>
-            <input type="text" name="email">
-            <a href="">Cadastrar</a><br>
-            <label for="">Senha:</label>
-            <input type="text" name="senha">
-            <button type="submit">Entrar</button>
-
-        </form>
-    </span>
+        </span>
 
     </div>
 
